450e4d2a4c3d91fac1406621976424a6c04c908d
navneetvats1998 <55892239+navneetvats1998@users.noreply.github.com>
2022-10-01 22:35:40 +0530

    commit
    commit

--- employee/src/app/components/not-found/not-found.component.ts
+++ employee/src/app/components/not-found/not-found.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup , FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
+signUpForm!:FormGroup;
+constructor( private auth :AuthService,private router: Router,
+  private formBuilder: FormBuilder) {
 
-  constructor() { }
+  this.createValidationForm();
+
+}
 
   ngOnInit(): void {
+    // if (this.auth.isLoggedIn()) {
+    //   this.router.navigate(['admin']);
+    // }
   }
+  createValidationForm() {
+    this.signUpForm = this.formBuilder.group({
+      email: [''],
+      password:[''],
+      role:['employee'],
+      username:[''],
+      contact:['']
+    })}
 
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value)
+      this.auth.Signup(this.signUpForm.value)
+    
+          this.router.navigate(['/login']);
+    debugger
+  console.log( localStorage.getItem('data')
+  
+  )   
+    }
+  }
 }
--- employee/src/app/services/auth/auth.service.ts
+++ employee/src/app/services/auth/auth.service.ts
@@ -5,7 +5,16 @@ import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthService {
+   users : any[]=[
+    { id: 1, name: 'Ram', email: 'ram@gmail.com', contact: '0000000000'  },
+    { id: 2, name: 'Shyam', email: 'sh@gmail.com', contact: '1111111111'  },
+    { id: 3, name: 'Mohan', email: 'moh@live.in', contact: '2222222222'  },
+    { id: 4, name: 'Rohan', email: 'rohan@gmail.com', contact: '6666666666' },
+    { id: 5, name: 'Sumit', email: 'sumit@live.in', contact: '9909999999'  }
+  
+  ];
   constructor(private router: Router) {}
 
   setToken(token: string): void {
@@ -27,9 +36,16 @@ export class AuthService {
 
   login({ email, password }: any): Observable<any> {
     if (email === 'admin@gmail.com' && password === 'admin123') {
-      this.setToken('abcdefghijklmnopqrstuvwxyz');
+      this.setToken('abcdefghi');
       return of({ name: 'Tarique Akhtar', email: 'admin@gmail.com' });
     }
     return throwError(new Error('Failed to login'));
+  }
+
+
+  Signup(form: []){
+localStorage.getItem('data');
+  console.log(this.users);
+  // localStorage.setItem('data' , a);
   }
 }
